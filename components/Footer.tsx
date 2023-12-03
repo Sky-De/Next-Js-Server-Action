b92b1471b6a63d410752c9f0fafce3cfed8fbb65
skyDe <sky_de1991@outlook.com>
2023-12-03 08:25:31 -0800

    hero refactored / footer icon changed / ready for production test

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Logo from "@/public/logo.png";
 
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <p className="text-base font-bold text-white">@2023 World Of Anime</p>
       <Image
-        src="./logo.svg"
+        src={Logo}
         alt="logo"
         width={47}
         height={44}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,22 +1,23 @@
 import Image from "next/image";
+import Logo from "@/public/logo.png";
 
 function Hero() {
   return (
-    <header className="bg-hero bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
+    <header className="bg-hero bg-center bg-cover bg-no-repeat bg-opacity-5 sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full h-screen sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
         <Image
-          src="./logo.svg"
+          src={Logo}
           alt="logo"
-          width={101}
-          height={96}
+          width={121}
+          height={109}
           className="object-contain"
         />
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Explore The <span className="red-gradient">Diverse Realms</span> of
-          Anime Magic
+          Unveiling Endless <span className="red-gradient">Anime Realms</span>{" "}
+          anime Magic
         </h1>
       </div>
-      <div className="lg:flex-1 relative w-full h-[50vh] justify-center">
+      <div className="lg:flex-1 relative w-full h-[70vh] justify-center">
         <Image src="/anime.png" alt="anime" fill className="object-contain" />
       </div>
     </header>
