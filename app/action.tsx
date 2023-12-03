cb940e521180aab5de31eab94e5e391edffb1b66
skyDe <sky_de1991@outlook.com>
2023-12-03 08:27:54 -0800

    rapid api removed from action file

--- app/action.tsx
+++ app/action.tsx
@@ -4,23 +4,8 @@ import { v4 as uuid } from "uuid";
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
 export const fetchAnime = async (page: number) => {
-  const rapidAPIKey = process.env.X_RapidAPI_Key;
-  const rapidAPIHost = process.env.X_RapidAPI_Host;
-
-  if (!rapidAPIKey || !rapidAPIHost) {
-    throw new Error("RapidAPI Key or Host is missing");
-  }
-
-  const headers = {
-    "X-RapidAPI-Key": rapidAPIKey,
-    "X-RapidAPI-Host": rapidAPIHost,
-  };
-
   const response = await fetch(
-    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`,
-    {
-      headers,
-    }
+    `https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`
   );
 
   const data = await response.json();
