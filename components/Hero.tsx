8dce31da090109855b660c09c0fd8e7c5aceb67e
skyDe <sky_de1991@outlook.com>
2023-12-06 10:32:35 -0800

    hero section responsiveness

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,7 +3,7 @@ import Logo from "@/public/logo.png";
 
 function Hero() {
   return (
-    <header className="bg-hero bg-center bg-cover bg-no-repeat bg-opacity-5 sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full h-screen sm:gap-16 gap-0">
+    <header className="bg-hero bg-center bg-cover bg-no-repeat bg-opacity-5 sm:p-16 py-16 px-8 flex justify-center lg:items-center flex-col sm:flex-row  w-full h-screen sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
         <Image
           src={Logo}
@@ -13,8 +13,9 @@ function Hero() {
           height={109}
           className="object-contain"
         />
-        <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
-          Unveiling Endless <span className="red-gradient">Anime Realms</span>{" "}
+        <h1 className=" text-2xl sm:text-3xl md:text-5xl lg:text-6xl text-white lg:max-w-lg font-bold leading-[120%]">
+          Unveiling Endless{" "}
+          <span className="block lg:inline red-gradient">Anime Realms</span>{" "}
           anime Magic
         </h1>
       </div>
